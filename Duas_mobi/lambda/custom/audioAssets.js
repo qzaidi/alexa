90ead25c28143542b248adbe898f43ccf4b0cd02
Qasim Zaidi <qasim@zaidi.me>
2017-11-26 07:51:10 +0700

    ask deploy for duas.mobi skill

---
+++ Duas_mobi/lambda/custom/audioAssets.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var audioData = [
+    {
+       'title': 'Dua Sahar',
+        'url': 'https://storage.googleapis.com/mp3duas/sahar.mp3'
+    },
+    {
+        'title' : 'Ziyarat al Arbaeen',
+        'url' : 'https://storage.googleapis.com/mp3duas/ziyarat/arbaeen.mp3'
+    },
+    {
+        'title' : 'Ziyarat al Warisa',
+        'url' : 'https://storage.googleapis.com/mp3duas/ziyarat/warisa.mp3'
+    }
+];
+
+module.exports = audioData;
